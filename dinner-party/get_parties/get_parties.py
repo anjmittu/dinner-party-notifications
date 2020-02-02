ea7ed6af676b4c46060f2ae641879fa6656bfee4
nmittu <nick@mittudev.com>
2020-02-01 19:51:53 -0500

    Use util lib

--- dinner-party/get_parties/get_parties.py
+++ dinner-party/get_parties/get_parties.py
@@ -1,6 +1,5 @@
 import os
 import time
-from google.cloud import pubsub_v1
 from dinner_party_database.utils import Utils
 import json
 
@@ -8,21 +7,6 @@ def get_groups(request):
     # This will get all the parties
     all_groups = Utils.get_all_party()
 
-    # Set up pub/sub system which will trigger other lambda
-    publisher = pubsub_v1.PublisherClient()
-    topic_path = publisher.topic_path(os.getenv("PROJECT_ID"), os.getenv("TOPIC_NAME"))
-
-    futures = dict()
-
-    def get_callback(f, data):
-        def callback(f):
-            try:
-                print(f.result())
-                futures.pop(data)
-            except:  # noqa
-                print("Please handle {} for {}.".format(f.exception(), data))
-
-        return callback
 
     # Loops through each party
     for party in all_groups:
@@ -37,12 +21,7 @@ def get_groups(request):
                     "last_question": 9
                 })
 
-                futures.update({data: None})
-
-                future = publisher.publish(topic_path, data=data.encode("utf-8"))
-                futures[data] = future
-
-                future.add_done_callback(get_callback(future, data))
+                Utils.trigger_function(data)
             return
 
         data = json.dumps({
@@ -52,14 +31,5 @@ def get_groups(request):
         })
         print("{} will be cooking".format(data))
 
-        futures.update({data: None})
-
-        future = publisher.publish(topic_path, data=data.encode("utf-8"))
-        futures[data] = future
-
-        future.add_done_callback(get_callback(future, data))
+        Utils.trigger_function(data)
     
-    while futures:
-        time.sleep(5)
-
-get_groups(None)
